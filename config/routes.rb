ab8189bbe3841dda7500b3b304e96ef2ab0e180b
rikenshah <rikenmanojshah@gmail.com>
2017-11-04 15:58:05 -0400

    Admin Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'application#index'
+ 	devise_for :users, controllers: {
+      sessions: 'users/sessions'
+  }
+  scope "/admin" do
+    get '/users/show', to: 'users#show'
+    post '/users/savenew', to: 'users#savenew'
+    resources :users
+  end
   resources :activities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104192804) do
+ActiveRecord::Schema.define(version: 20171104194847) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20171104192804) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.boolean "superadmin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
